081cd71fd671f18fdf873410273966a66c1f73cb
Kalle Ott <kalle.ott@cap3.de>
2018-05-15 09:56:18 +0200

    improved color generator

--- src/utils/colorHelpers.js
+++ src/utils/colorHelpers.js
@@ -1,24 +1,21 @@
-export const hashCode = str => {
-  // java String#hashCode
+import materialpalette from "material-palette";
+
+const hashCode = string => {
   let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+
+  for (let char of string) {
+    hash = ((hash << 5) - hash + char.charCodeAt()) | 0;
   }
   return hash;
 };
 
-export const intToRGB = i => {
-  const c = (i & 0x00ffffff).toString(16).toUpperCase();
-
-  return "00000".substring(0, 6 - c.length) + c;
-};
-
-const between = (min, max) => val => Math.max(min, Math.min(val, max));
-
 export const generateColor = str => {
-  const hash = hashCode(str) & 0x0fffffff;
-  var h = between(0, 360)(hash % 360);
-  var s = between(52, 87)(hash & (0x0000ff00 >> 8));
-  var l = between(10, 40)(hash & 0x000000ff);
-  return `hsl(${h},${s}%,${l}%)`;
+  const hash = hashCode(str) * 11.25;
+  const h = Math.abs(hash % 360);
+  const s = 58;
+  const l = 54;
+
+  const color = materialpalette({ h, s, l })["A400"];
+
+  return `hsl(${color.h},${color.s}%,${color.l}%)`;
 };
